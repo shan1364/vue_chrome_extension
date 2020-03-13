75a15d001c927555f6f46a4660e7835d2036ba44
Yan <yanshan1364@gmail.com>
2020-03-13 17:33:03 +0800

    [Feature] Add fetch comment function

--- src/popup/App.vue
+++ src/popup/App.vue
@@ -12,7 +12,7 @@
                 class="form-control"
                 id="inputArea"
                 placeholder="facebook.com/profile.ph.?id=example"
-                rows="5"
+                rows="3"
                 v-model="inputLinks"
                 style="word-wrap : break-word;"
               ></textarea>
@@ -20,19 +20,19 @@
           </form>
         </div>
       </div>
-      <p>結果:</p>
-      <table class="table">
+      <p>Profile結果:</p>
+      <table class="table" id="profileTable">
         <thead>
           <tr class="table-active">
             <th data-field="id">ID</th>
-            <th data-field="link" colspan="2">link</th>
+            <th data-field="link" colspan="4">link</th>
             <th data-field="progress">progress</th>
           </tr>
         </thead>
         <tbody>
-          <tr v-for="item in checkShow(objectData)" :key="item">
+          <tr v-for="item in showProfileList(profileData)" :key="item">
             <th>{{ item.id }}</th>
-            <th colspan="2" style="word-break:break-all;">{{ item.link }}</th>
+            <th colspan="4" style="word-break:break-all;">{{ item.link }}</th>
             <th>
               <span class="fui-check" v-if="item.data !== ''" style="color:#32CD32;"></span>
             </th>
@@ -40,19 +40,37 @@
         </tbody>
       </table>
 
-      <div class="row">
-        <div class="col">
-          <button class="btn btn-primary" v-on:click="saveInputLink">
-            <span class="fui-search"></span> 送出連結
-          </button>
-        </div>
-        <div class="col"></div>
-        <div class="col">
-          <button class="btn btn-primary" v-on:click="downloadData">
-            <span class="fui-clip"></span> 下載結果
-          </button>
-        </div>
-      </div>
+      <!-- 留言區塊 -->
+      <p>Comment結果:</p>
+      <table class="table" id="commentTable">
+        <thead>
+          <tr class="table-active">
+            <th data-field="id2">ID</th>
+            <th data-field="user">user</th>
+            <th colspan="4" data-field="content">comment</th>
+            <th data-field="level">level</th>
+          </tr>
+        </thead>
+        <tbody>
+          <tr v-for="item in showCommentList(commentData)" :key="item">
+            <th>{{ item.id }}</th>
+            <th>{{ item.user }}</th>
+            <th colspan="4" style="word-break:break-all;">{{ item.content }}</th>
+            <th>{{ item.level }}</th>
+          </tr>
+        </tbody>
+      </table>
+
+      <button class="button" id="button" v-on:click="saveInputLink">
+        <span class="fui-search"></span> 送出連結
+      </button>
+      <button class="button" id="downloadBtn" v-on:click="downloadProfileData">
+        <span class="fui-clip"></span> 下載結果
+      </button>
+
+      <button class="button" id="commentBtn" v-on:click="fetchComment">
+        <span class="fui-bubble"></span> 取得留言
+      </button>
     </div>
   </div>
 </template>
@@ -65,81 +83,80 @@ const { Parser } = require("json2csv");
 export default {
   data() {
     return {
-      objectData: {}
+      profileData: {},
+      commentData: {}
     };
   },
 
   methods: {
     saveInputLink: function() {
-      console.log("start to save origin links");
-      //清除儲存區
-      console.log("data:", this.inputLinks);
-      chrome.storage.sync.clear(function() {});
+      console.log("start to save origin  input links");
+      //清除儲存區中的 個人介紹物件
+      console.log("input links:", this.inputLinks);
+      chrome.storage.sync.set({ profile: { data: "", state: "" } }, function() {});
       //處理連結
-      let links = this.inputLinks.split(",");
-      var arr = [];
-      var j = 1;
-      for (let i = 0; i < links.length; i++) {
-        let item_obj = {};
-        let url_pattern = "www.facebook.com";
-        if (links[i].search(url_pattern) >= 0) {
-          let id = j;
-          item_obj.id = id;
-          item_obj.link = links[i].replace(/(\r\n|\n|\r|)/gm, "");
-          item_obj.data = "";
-          item_obj.state = "";
-          arr.push(item_obj);
-          j++;
-        }
-      }
-      let len = arr.length;
-      arr.push({ len: len });
-      let obj = { ...arr };
-      console.log(obj);
-      this.objectData = obj;
-      console.log("vue data:", this.objectData);
-      //寫入儲存區
-      chrome.storage.sync.set(obj, function() {
-        console.log("save origin links finish");
-      });
-    },
-
-    downloadData: function() {
-      chrome.storage.sync.get(null, function(data) {
-        let origin_item = data;
-        let keys = Object.keys(origin_item);
-        let output_array = [];
-        for (const key of keys) {
-          if (
-            !origin_item[key].hasOwnProperty("len") &&
-            origin_item[key].data !== ""
-          ) {
-            output_array.push(origin_item[key]);
+      //console.log('this.inputLinks:', this.inputLinks);
+      if (this.inputLinks !== null || this.inputLinks.trim() !== "") {
+        let links = this.inputLinks.split(",");
+        var link_arr = [];
+        var j = 1;
+        for (let i = 0; i < links.length; i++) {
+          let item_obj = {};
+          let url_pattern = "www.facebook.com";
+          if (links[i].search(url_pattern) >= 0) {
+            let id = j;
+            item_obj.id = id;
+            item_obj.link = links[i].replace(/(\r\n|\n|\r)/gm, "");
+            item_obj.data = "";
+            item_obj.state = "";
+            link_arr.push(item_obj);
+            j++;
           }
         }
-        const fields = [
-          {
-            label: "id",
-            value: "id"
-          },
-          { label: "link", value: "link" },
-          { label: "user_ID", value: "data.ID" },
-          { label: "user_name", value: "data.name" },
-          { label: "user_img", value: "data.img" }
-        ];
-        const json2csvParser = new Parser({ fields });
-        const csv = json2csvParser.parse(output_array);
-        console.log(csv);
-        var hiddenElement = document.createElement("a");
-        //encodeURI用來轉為UTF-8編碼
-        hiddenElement.href = "data:text/csv;charset=utf-8,\uFEFF" + encodeURI(csv);
-        hiddenElement.target = "_blank";
-        hiddenElement.download = "link_profileData.csv";
-        hiddenElement.click();
+        let len = link_arr.length;
+        link_arr.push({ len: len });
+        let link_obj = { ...link_arr };
+        console.log("input links:", link_obj);
+        this.profileData = link_obj;
+        console.log("Vue profile data:", this.profileData);
+        //寫入 個人介紹 的儲存區
+        chrome.storage.sync.set({ profile: { data: link_obj } }, function() {
+          console.log("save origin links finish");
+        });
+      }
+    },
+
+    downloadProfileData: function() {
+      chrome.storage.sync.get("profile", function(item) {
+        let origin_item = item.profile.data;
+        if (!origin_item[0].hasOwnProperty("len")) {
+          let keys = Object.keys(origin_item);
+          let output_array = [];
+          for (const key of keys) {
+            if ( !origin_item[key].hasOwnProperty("len") && origin_item[key].data !== "") output_array.push(origin_item[key]);
+          }
+          const fields = [
+            { label: "id", value: "id" },
+            { label: "link", value: "link" },
+            { label: "user_ID", value: "data.ID" },
+            { label: "user_name", value: "data.name" },
+            { label: "user_img", value: "data.img" }
+          ];
+          const json2csvParser = new Parser({ fields });
+          const csv = json2csvParser.parse(output_array);
+          console.log(csv);
+          var hiddenElement = document.createElement("a");
+          //encodeURI用來轉為UTF-8編碼
+          hiddenElement.href =
+            "data:text/csv;charset=utf-8,\uFEFF" + encodeURI(csv);
+          hiddenElement.target = "_blank";
+          hiddenElement.download = "link_rofile_data.csv";
+          hiddenElement.click();
+        }
       });
     },
 
-    checkShow: function(items) {
+    showProfileList: function(items) {
       let origin_item = items;
       var keys = Object.keys(origin_item);
       let arr = [];
@@ -149,68 +166,98 @@ export default {
         }
       }
       return arr;
+    },
+
+    showCommentList: function(items) {
+      let origin_item = items;
+      var keys = Object.keys(origin_item);
+      let arr = [];
+      for (const key of keys) {
+        arr.push(origin_item[key]);
+      }
+      return arr;
+    },
+
+    fetchComment: function() {
+      //清除儲存區中的 留言物件
+      chrome.storage.sync.set({comment: { state: "", data: "" }}, function() {});
+      //寫入儲存區key:comment的value
+      chrome.storage.sync.set({ comment: { state: "fetch_comment", data: "" } }, function() {});
     }
   },
 
   mounted: function() {
     var _this = this;
+    //掛載就開始設定資料
+    chrome.storage.sync.set(
+      { comment: { state: "", data: "" }, profile: { data: "", state: "" } },
+      function() {
+        console.log("set initial");
+      }
+    );
 
     chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
       //更新storage的資料 並(1)到還沒去過的新頁面 或 (2)完成 印出
       chrome.storage.onChanged.addListener(function(changes, namespace) {
-        chrome.storage.sync.get(null, function(data) {
-          //更新資料到 vue
-          _this.objectData = data;
+        console.log("changes:", changes);
+        var storage_keys = Object.keys(changes);
+        for (const storage_key of storage_keys) {
+          if (storage_key == "comment") {
+            //更改的是 留言區
+            chrome.storage.sync.get("comment", function(item) {
+              _this.commentData = item.comment.data;
+            });
+          } else if (storage_key == "profile") {
+            //更改的是 個人頁面區
+            chrome.storage.sync.get("profile", function(item) {
+              //更新profile資料到 vue
+              _this.profileData = item.profile.data;
 
-          let origin_item = data;
-          var keys = Object.keys(origin_item);
-          for (var key in changes) {
-            var storageChange = changes[key];
-            console.log(
-              'Storage key "%s" in namespace "%s" changed. Old value was "%s", new value is "%s".',
-              key,
-              namespace,
-              storageChange.oldValue,
-              storageChange.newValue
-            );
-          }
-          for (const key of keys) {
-            //data=='' 代表要去新的連結
-            if (
-              !origin_item[key].hasOwnProperty("len") &&
-              origin_item[key].data == ""
-            ) {
-              let goto_url = origin_item[key].link;
-              console.log("ready to:", goto_url);
-              chrome.tabs.update({ url: goto_url });
-              break;
-            } else if (origin_item[key].hasOwnProperty("len")) {
-              //全部完成 開始output
-              let output_items = data;
-              let keys = Object.keys(output_items);
+              let origin_item = item.profile.data;
+              var keys = Object.keys(origin_item);
+
               for (const key of keys) {
-                if (!output_items[key].hasOwnProperty("len")) {
-                  let item_data = output_items[key].data;
-                  let ID = item_data.ID;
-                  let name = item_data.name;
-                  let img = item_data.img;
-                  let items = { ID, name, img };
-                  console.log("output item:", items);
-                  //output_area.value += JSON.stringify(items) + ', ';
+                //data=='' 代表要去新的連結
+                if (
+                  !origin_item[key].hasOwnProperty("len") &&
+                  origin_item[key].data == ""
+                ) {
+                  let goto_url = origin_item[key].link;
+                  console.log("ready to:", goto_url);
+                  chrome.tabs.update({ url: goto_url });
+                  break;
+                } else if (
+                  !origin_item[0].hasOwnProperty("len") &&
+                  origin_item[key].hasOwnProperty("len")
+                ) {
+                  //全部完成 開始output
+                  let output_items = origin_item;
+                  let keys = Object.keys(output_items);
+                  for (const key of keys) {
+                    if (!output_items[key].hasOwnProperty("len")) {
+                      let item_data = output_items[key].data;
+                      let ID = item_data.ID;
+                      let name = item_data.name;
+                      let img = item_data.img;
+                      let items = { ID, name, img };
+                      console.log("output item:", items);
+                    }
+                  }
+                  console.log("output all finish");
+                  break;
                 }
               }
-              console.log("output all finish");
-              break;
-            }
+            });
           }
-        });
+        }
       });
 
       //確認已經到 新的連結
       chrome.tabs.onUpdated.addListener(function(tabID, changeInfo, tab) {
+        //console.log('tabs:', tabs, 'tabID:', tabID);
         if (changeInfo.status == "complete") {
-          chrome.storage.sync.get(null, function(data) {
-            let origin_item = data;
+          chrome.storage.sync.get("profile", function(item) {
+            let origin_item = item.profile.data;
             var keys = Object.keys(origin_item);
             for (const key of keys) {
               if (
@@ -221,9 +268,12 @@ export default {
                 console.log(
                   "origin item:",
                   origin_item[key],
-                  "start to fetch data"
+                  "start to fetch profile data"
                 );
-                chrome.storage.sync.set(origin_item, function() {});
+                chrome.storage.sync.set(
+                  { profile: { data: origin_item } },
+                  function() {}
+                );
                 break;
               }
             }
@@ -239,19 +289,5 @@ export default {
 #app {
   width: 500px;
   font-family: Microsoft JhengHei;
-}
-.container {
-  background-color: #fafafa;
-}
-
-hr.style-one {
-  border: 0;
-  height: 1px;
-  background-image: linear-gradient(
-    to right,
-    rgba(0, 0, 0, 0),
-    rgba(0, 0, 0, 0.75),
-    rgba(0, 0, 0, 0)
-  );
 }
 </style>
